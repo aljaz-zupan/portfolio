b8b04096ea5444c2947dd2384a0bad0100284d13
Aljaž <aljaz.zupan@gmail.com>
2022-09-29 23:06:04 +0200

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,42 +6,7 @@
 <div class="drawer">
 	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col">
-		<!-- Navbar -->
-		<div class="w-full navbar bg-base-300">
-			<div class="flex-1 px-2 mx-2">Aljaz.dev</div>
-			<divj class="flex-none hidden lg:block">
-				<ul class="menu menu-horizontal">
-					<!-- Navbar menu content here -->
-					<li class="menu-title">
-						<span>MAIN</span>
-					</li>
-					<li class="bordered"><a>O meni</a></li>
-					<li><a>Kontakt</a></li>
-					<li><a>Blog</a></li>
-					<li class="menu-title">
-						<span>DEV</span>
-					</li>
-					<li><a>Komponente</a></li>
-					<li><a>Dodatno</a></li>
-				</ul>
-			</divj>
-			<div class="flex-none lg:hidden">
-				<label for="my-drawer-3" class="btn btn-square btn-ghost">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						class="inline-block w-6 h-6 stroke-current"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4 6h16M4 12h16M4 18h16"
-						/></svg
-					>
-				</label>
-			</div>
-		</div>
+		<Header />
 		<main>
 			<slot />
 		</main>
@@ -52,50 +17,37 @@
 			</div>
 			<div>
 				<div class="grid grid-flow-col gap-4">
-					<a
+					<a href="https://github.com/aljaz-zupan"
 						><svg
-							xmlns="http://www.w3.org/2000/svg"
 							width="24"
 							height="24"
+							stroke-width="0.96"
 							viewBox="0 0 24 24"
-							class="fill-current"
-							><path
-								d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-							/></svg
-						></a
-					>
-					<a
-						><svg
+							fill="none"
 							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
 							class="fill-current"
 							><path
-								d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-							/></svg
-						></a
-					>
-					<a
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							class="fill-current"
-							><path
-								d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+								d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+								stroke="#000000"
+								stroke-width="0.96"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/><path
+								d="M14.333 19v-1.863c.025-.31-.018-.62-.126-.913a2.18 2.18 0 00-.5-.781c2.093-.227 4.293-1 4.293-4.544 0-.906-.358-1.778-1-2.434a3.211 3.211 0 00-.06-2.448s-.787-.227-2.607.961a9.152 9.152 0 00-4.666 0c-1.82-1.188-2.607-.96-2.607-.96A3.211 3.211 0 007 8.464a3.482 3.482 0 00-1 2.453c0 3.519 2.2 4.291 4.293 4.544a2.18 2.18 0 00-.496.773 2.134 2.134 0 00-.13.902V19M9.667 17.702c-2 .631-3.667 0-4.667-1.948"
+								stroke="#000000"
+								stroke-width="0.96"
+								stroke-linecap="round"
+								stroke-linejoin="round"
 							/></svg
 						></a
 					>
 				</div>
 			</div>
 			<div>
-				<p>Copyright © 2022 - vse pravice pridržane Aljaž Zupan</p>
+				<p>© 2022 Aljaž Zupan</p>
 			</div>
 		</footer>
 	</div>
-
 	<div class="drawer-side">
 		<label for="my-drawer-3" class="drawer-overlay" />
 		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
@@ -103,14 +55,10 @@
 			<li class="menu-title">
 				<span>MAIN</span>
 			</li>
-			<li class="bordered"><a>O meni</a></li>
-			<li><a>Kontakt</a></li>
-			<li><a>Blog</a></li>
-			<li class="menu-title">
-				<span>DEV</span>
-			</li>
-			<li><a>Komponente</a></li>
-			<li><a>Dodatno</a></li>
+			<li class="bordered"><a href="/about">O meni</a></li>
+			<li><a href="/contact">Kontakt</a></li>
+			<li><a href="/blog">Blog</a></li>
+			<li><a href="/projects">Projekti</a></li>
 		</ul>
 	</div>
 </div>
