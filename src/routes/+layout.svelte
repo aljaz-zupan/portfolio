6f8a4fecb4a7719a04b12af1a09fb6fe47484341
Aljaž <aljaz.zupan@gmail.com>
2022-10-09 15:52:25 +0200

    crated floating menu button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from '$lib/header/Header.svelte';
+
+	let winWidth;
 </script>
+
+<svelte:window bind:innerWidth={winWidth} />
 
 <div class="drawer">
 	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col">
-		<Header />
+		<!-- <Header /> -->
 		<main>
 			<slot />
 		</main>
@@ -43,11 +47,13 @@
 					>
 				</div>
 			</div>
+
 			<div>
 				<p>© 2022 Aljaž Zupan</p>
 			</div>
 		</footer>
 	</div>
+
 	<div class="drawer-side">
 		<label for="my-drawer-3" class="drawer-overlay" />
 		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
@@ -59,6 +65,85 @@
 			<li><a href="/contact">Kontakt</a></li>
 			<li><a href="/blog">Blog</a></li>
 			<li><a href="/projects">Projekti</a></li>
+		</ul>
+	</div>
+</div>
+<div class="menu absolute bottom-4 right-4 lg:top-8 lg:left-8">
+	<div
+		class="m-6 dropdown"
+		class:dropdown-top={winWidth < 1024}
+		class:dropdown-end={winWidth < 1024}
+	>
+		<label tabindex="0" class="btn btn-ghost bg-primary hover:bg-primary-focus btn-circle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h7"
+				/></svg
+			>
+		</label>
+		<ul
+			tabindex="0"
+			class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box"
+		>
+			<li>
+				<a href="/">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+						/></svg
+					>
+				</a>
+			</li>
+			<li>
+				<a href="/blog">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+				</a>
+			</li>
+			<li>
+				<a href="/about">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+						/></svg
+					>
+				</a>
+			</li>
 		</ul>
 	</div>
 </div>
