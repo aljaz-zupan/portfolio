45b7ddc440b7ab0d7e8d4172cadc1fd69913373e
Aljaž <aljaz.zupan@gmail.com>
2022-12-26 10:07:58 +0100

    added test to unify structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<ul>
+	<li><a href="/projects">Projects</a></li>
+</ul>
+
 <h1>Welcome to my portfolio</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
