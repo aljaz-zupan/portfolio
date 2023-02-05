69ee63ea2a380722187a19d44404f329219b654d
alzupan <aljaz.zupan@telekom.si>
2023-02-06 00:02:53 +0100

    remove unvanted components

--- src/lib/CodeBlock/Code.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Highlight from 'svelte-highlight';
-	import typescript from 'svelte-highlight/languages/typescript';
-
-	const code = 'const add = (a: number, b: number) => a + b;';
-</script>
-
-<pre><code><slot /></code></pre>
