d6156fb07b6249905740a1d2e657199278518dc7
Aljaž <aljaz.zupan@gmail.com>
2023-02-07 17:20:27 +0100

    some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 
 <h1>Welcome to my portfolio</h1>
 <p>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quaerat rem maiores sit dolorum
-	repellendus voluptas necessitatibus est architecto officiis.
+	Hello there! I'm a code wizard, caffeine addict, and occasional comedian. My keyboard and I have
+	been conjuring up websites, applications, and software like magic. I may not be able to summon a
+	unicorn, but I can make a website load faster than one. So sit back, relax, and enjoy the show as
+	I present to you my skills, experience, and probably a few jokes.
 </p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,8 @@
 <h1>Contact</h1>
 
-<p>the is some text</p>
+<p>There are multiple ways you can contact me.</p>
+<ul>
+	<li><a href="mailto:aljaz.zupan@gmail.com">Email</a></li>
+	<li><a href="https://www.linkedin.com/in/aljaz-zupan">LinkedIn</a></li>
+	<li><a href="https://github.com/aljaz-zupan">Github</a></li>
+</ul>
