ec9fb1718b37b1bcb088412a938f7d37f47b9858
alzupan <aljaz.zupan@telekom.si>
2023-02-08 19:19:11 +0100

    added hero height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts"></script>
 
-<div class="hero bg-gradient-to-r from-cyan-900 to-purple-900">
-	<div class="prose md:w-full md:mx-auto md:max-w-3xl px-4 md:px-0 py-4 text-left">
+<div class="hero bg-gradient-to-r from-cyan-900 to-purple-900 min-h-screen">
+	<div class="prose md:w-full md:mx-auto md:max-w-3xl px-4 md:px-0 py-4 text-center">
 		<h1>Aljaž's studio</h1>
-		<h4>Where coffine addiction begins</h4>
-		<p>Step into a universe where caffine, development, technoligies and geekdom unite.</p>
+		<h4>Where caffine addiction begins</h4>
+		<p>See where caffine, development, technologies and attemps to comedy merge in one place.</p>
 	</div>
 </div>
 <div class="prose md:w-full md:mx-auto md:max-w-3xl px-4 md:px-0 py-4 text-left">
@@ -14,6 +14,10 @@
 		been conjuring up websites, applications, and software like magic. I may not be able to summon a
 		unicorn, but I can make a website load faster than one. So sit back, relax, and enjoy the show
 		as I present to you my skills, experience, and probably a few jokes.
+	</p>
+	<p>
+		some more Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos facilis amet architecto!
+		Veritatis eius exercitationem ab repellendus reprehenderit esse. Eveniet!
 	</p>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </div>
