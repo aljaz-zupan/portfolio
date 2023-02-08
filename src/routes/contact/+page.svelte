745e4ecef25f98fa4e00947ba8cf659aa65d76db
Aljaž <aljaz.zupan@gmail.com>
2023-02-08 17:18:31 +0100

    added ko-fi image

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,3 +6,14 @@
 	<li><a href="https://www.linkedin.com/in/aljaz-zupan">LinkedIn</a></li>
 	<li><a href="https://github.com/aljaz-zupan">Github</a></li>
 </ul>
+
+<p>
+	Or just <a rel="noreferrer" href="https://ko-fi.com/L4L2HM2EA" target="_blank"
+		><img
+			height="36"
+			style="border:0px;height:36px; display: inline; margin-left: 1rem"
+			src="https://storage.ko-fi.com/cdn/kofi1.png?v=3"
+			alt="Buy Me a Coffee at ko-fi.com"
+		/></a
+	>
+</p>
