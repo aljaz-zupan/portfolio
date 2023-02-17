6baedf8d0e9f963495123e00ec5c47b2a73eed96
Aljaž <aljaz.zupan@gmail.com>
2023-02-17 18:55:48 +0100

    added google form integration

--- src/lib/menu/Logo.svelte
+++ src/lib/menu/Logo.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	export let title: string = 'ASudio';
+	export let title: string = 'AZ';
 	const titleArray = title.split('');
 </script>
 
 <a href="/" aria-current="page" aria-label="Homepage" class="flex-0 btn btn-ghost px-2"
 	><div class="font-title text-primary inline-flex text-lg transition-all duration-200 md:text-4xl">
-		<span class="lowercase absolute left-6 mix-blend-color bg-primary">{titleArray[0]}</span>
-		<span class="text-base-content uppercase mix-blend-color bg-secondary">{titleArray[1]}</span>
+		<span class="lowercase left-6">{titleArray[0]}</span>
+		<span class="text-base-content uppercase">{titleArray[1]}</span>
 	</div></a
 >
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,97 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+	export let form: ActionData;
+
+	$: console.log(form);
+</script>
+
 <h1>Contact</h1>
 
-<p>There are multiple ways you can contact me.</p>
+<form method="POST" use:enhance>
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="name">
+			<span class="label-text">What is your name?</span>
+		</label>
+		<input
+			type="text"
+			placeholder="Fireship"
+			class="input input-bordered w-full max-w-xs"
+			name="name"
+			id="name"
+			value={form?.data?.name ?? ''}
+		/>
+		{#if form?.errors?.name}
+			<label class="label" for="name">
+				<span class="label-text-alt text-error">{form?.errors?.name[0]}</span>
+			</label>
+		{/if}
+	</div>
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="email">
+			<span class="label-text">What is your email or any other contact info?</span>
+		</label>
+		<input
+			type="text"
+			placeholder="me@me.me"
+			class="input input-bordered w-full max-w-xs"
+			name="email"
+			id="email"
+			value={form?.data?.email ?? ''}
+		/>
+		{#if form?.errors?.email}
+			<label class="label" for="email">
+				<span class="label-text-alt text-error">{form?.errors?.email[0]}</span>
+			</label>
+		{/if}
+	</div>
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="message">
+			<span class="label-text">What is your name?</span>
+		</label>
+		<textarea
+			class="textarea textarea-bordered h-24"
+			name="message"
+			id="message"
+			value={form?.data?.message ?? ''}
+			placeholder="This is my message"
+		/>
+		{#if form?.errors?.message}
+			<label class="label" for="name">
+				<span class="label-text-alt text-error">{form?.errors?.message[0]}</span>
+			</label>
+		{/if}
+	</div>
+	{#if form?.message}
+		<div class="alert alert-{form?.type} shadow-lg mt-4">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="stroke-current flex-shrink-0 h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>{form?.message}</span>
+			</div>
+		</div>
+	{/if}
+	<div class="form-control w-full max-w-xs my-4">
+		<button class="btn btn-primary" type="submit">Submit</button>
+	</div>
+</form>
+
+<p>There are also other ways you can contact me.</p>
 <ul>
 	<li><a href="mailto:aljaz.zupan@gmail.com">Email</a></li>
 	<li><a href="https://www.linkedin.com/in/aljaz-zupan">LinkedIn</a></li>
 	<li><a href="https://github.com/aljaz-zupan">Github</a></li>
 </ul>
-
 <p>
 	Or just <a rel="noreferrer" href="https://ko-fi.com/L4L2HM2EA" target="_blank"
 		><img
