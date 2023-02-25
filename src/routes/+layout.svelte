1a951976f69f44c5660c755392ac03c5c9805630
alzupan <aljaz.zupan@telekom.si>
2023-02-25 20:24:15 +0100

    added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,20 +4,23 @@
 	import '../app.css';
 	import Navbar from '$lib/menu/Navbar.svelte';
 	import Drawer from '$lib/menu/Drawer.svelte';
+	import Footer from '$lib/footer/Footer.svelte';
 </script>
 
 <div class="drawer drawer-end">
 	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col">
+	<div class="drawer-content flex flex-col min-h-screen">
 		<Navbar />
+
 		<!-- Page content here -->
 		{#if $page.route.id === '/'}
 			<slot />
 		{:else}
-			<div class="prose md:w-full md:mx-auto md:max-w-3xl px-4 md:px-0 py-4 text-left">
+			<div class="prose md:w-full md:mx-auto md:max-w-3xl px-4 md:px-0 py-4 text-left min-h-full">
 				<slot />
 			</div>
 		{/if}
+		<Footer />
 	</div>
 	<Drawer />
 </div>
