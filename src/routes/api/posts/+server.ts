fe13f8248731f420d72d18846d26a30773ca2ffe
alzupan <aljaz.zupan@telekom.si>
2023-05-20 18:37:22 +0200

    added configuration and posts from md files

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -5,22 +5,22 @@ async function getPosts() {
 	let posts: Post[] = [];
 
 	const paths = import.meta.glob('/src/posts/*.md', { eager: true });
-	console.log(paths);
+	console.log('paths', paths);
 
 	for (const path in paths) {
 		const file = paths[path];
 		const slug = path.split('/').at(-1)?.replace('.md', '');
 
-		if (file && typeof file === 'object' && metadata in file && slug) {
+		if (file && typeof file === 'object' && 'metadata' in file && slug) {
 			const metadata = file.metadata as Omit<Post, 'slug'>;
 			const post = { ...metadata, slug } satisfies Post;
 			post.published && posts.push(post);
 		}
 	}
 
-	posts = posts.sort((first: Post, second: Post) => {
-		new Date(second.date).getTime() - new Date(first.date).getTime();
-	});
+	posts = posts.sort(
+		(first, second) => new Date(second.date).getTime() - new Date(first.date).getTime()
+	);
 
 	return posts;
 }
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href={post.slug} class="title">{post.title}</a>
+				<p class="date">{post.date}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
