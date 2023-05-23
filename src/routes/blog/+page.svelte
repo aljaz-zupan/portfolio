1c2cb8565387f0cfe1155f573ad1b1bc0d8dcad0
alzupan <aljaz.zupan@telekom.si>
2023-05-23 05:08:16 +0200

    added working single posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import * as config from '$lib/config';
-
+	import { formatDate } from '$lib/utils.js';
 	export let data;
 </script>
 
@@ -13,9 +13,16 @@
 		{#each data.posts as post}
 			<li class="post">
 				<a href={post.slug} class="title">{post.title}</a>
-				<p class="date">{post.date}</p>
+				<p class="date">{formatDate(post.date)}</p>
 				<p class="description">{post.description}</p>
 			</li>
 		{/each}
 	</ul>
 </section>
+
+<style>
+	li:not(:last-child) {
+		margin-bottom: 2rem;
+		border-bottom: 1px solid grey;
+	}
+</style>
