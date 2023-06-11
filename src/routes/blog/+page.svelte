ca2973dd05f79e7eea517a522a2827859a45c3c2
alzupan <aljaz.zupan@telekom.si>
2023-06-11 15:10:50 +0200

    somethinghs #4

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,28 @@
-<h2>Blog</h2>
+<script lang="ts">
+	import * as config from '$lib/config';
+	import { formatDate } from '$lib/utils.js';
+	export let data;
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href={post.slug} class="title">{post.title}</a>
+				<p class="date">{formatDate(post.date)}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	li:not(:last-child) {
+		margin-bottom: 2rem;
+		border-bottom: 1px solid grey;
+	}
+</style>
