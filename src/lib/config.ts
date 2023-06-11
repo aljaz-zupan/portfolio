ce4e5366a77821cd8ba22c825bc88052bfec0253
alzupan <aljaz.zupan@telekom.si>
2023-06-11 15:16:42 +0200

    somethinghs #8

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'AljažDev';
+export const description = 'Sveltkit portfolio and experimentation page';
+export const url = dev ? 'http://localhost:5173/' : 'portfolio-aljaz-zupan.vercel.app/';
