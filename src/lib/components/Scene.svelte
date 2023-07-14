3f3fafc603f2909ce301e0e01b67b182c06600db
aljaz-zupan <aljaz.zupan@gmail.com>
2023-07-14 20:46:59 +0200

    added color geter from theme

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -3,6 +3,17 @@
 	import { Grid, ContactShadows } from '@threlte/extras';
 	import { MeshBasicMaterial, SphereGeometry } from 'three';
 	import Camera from './Camera.svelte';
+
+	const css_variable = document.querySelector(':root');
+
+	function return_HSL_value(variable: string = '--color-primary-500'): string {
+		if (css_variable === null) return '';
+		const value = getComputedStyle(css_variable);
+		const variable_value = value.getPropertyValue(variable);
+		// console.log('variable value', variable_value);
+		const [h, s, l] = variable_value.split(' ');
+		return `hsl(${h}, ${s}%, ${l}%)`;
+	}
 </script>
 
 <Camera />
@@ -10,7 +21,7 @@
 <Grid
 	position.y={-0.001}
 	cellColor="#ffffff"
-	sectionColor="#d4163c"
+	sectionColor={return_HSL_value('--color-primary-500')}
 	sectionThickness={2}
 	fadeDistance={30}
 	fadeStrength={2}
@@ -22,6 +33,6 @@
 
 <T.Mesh
 	position={[0, 3, 0]}
-	material={new MeshBasicMaterial({ wireframe: true, color: '#fff' })}
+	material={new MeshBasicMaterial({ wireframe: true, color: 'hsl(212, 22%, 60%)' })}
 	geometry={new SphereGeometry(3, 22, 20)}
 />
